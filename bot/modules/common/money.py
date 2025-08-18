07b18af41400c557b32bd6de17dd430391fc4e0e
martin <riembault.martin@gmail.com>
2025-08-18 21:35:07 +0200

    v1

--- bot/modules/common/money.py
+++ bot/modules/common/money.py
@@ -15,4 +15,4 @@ def fmt_eur(cents: int) -> str:
     euros = s // 100
     cents_part = abs(s) % 100
     sign = "-" if s < 0 else ""
-    return f"{sign}{euros},{cents_part:02d}{MONEY_EMOJI}"
+    return f"{sign}{euros},{cents_part:02d} {MONEY_EMOJI}"
